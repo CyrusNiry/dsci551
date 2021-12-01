03d7e741dae1cb2718a8a394253ff5b9745ddf0c
CyrusNiry <78511858+CyrusNiry@users.noreply.github.com>
2021-12-02 06:04:46 +0800

    Update q3_a.py

--- q3_a.py
+++ q3_a.py
@@ -2,7 +2,5 @@ from pyspark import SparkContext
 from operator import add
 sc=SparkContext(appName='dsci551 q3')
 film_lines=sc.textFile('film.csv')
-counts = film_lines.map(lambda x: x.split(',')).filter(lambda List: List[10]=='"PG"' or List[10]=='"PG-13"').map(lambda List: (List[10], 1)).count()
-a=[counts]
-for i in a:
-    print(i)
+count = film_lines.map(lambda x: x.split(',')).filter(lambda List: List[10]=='"PG"' or List[10]=='"PG-13"').map(lambda List: (List[10], 1)).count()
+print(count)
